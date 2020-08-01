f8f820d37545e62413c47421d9b10e2c8129ac1c
gabriela-ciurea <ciurea.gabriela01@gmail.com>
2020-08-01 15:05:03 +0300

    fix bug

--- src/test/java/com/kronsoft/medicaloffice/controller/ControllerBaseIT.java
+++ src/test/java/com/kronsoft/medicaloffice/controller/ControllerBaseIT.java
@@ -9,9 +9,9 @@ import com.kronsoft.medicaloffice.persistence.repositories.MedicalProcedureRepos
 import com.kronsoft.medicaloffice.persistence.repositories.PatientRepository;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -20,11 +20,11 @@ import java.time.Month;
 
 @ActiveProfiles("test")
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public abstract class ControllerBaseIT {
 
   @Autowired protected TestRestTemplate restTemplate;
-  @Value("${server.port}") protected int port;
+  @LocalServerPort protected int port;
 
   @Autowired protected PatientRepository patientRepository;
   @Autowired protected DoctorRepository doctorRepository;
